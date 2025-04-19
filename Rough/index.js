be334c868dede4373bc534eafe84257944fa1f19
Praveendoss1 <praveendoss890@gmail.com>
2025-04-19 11:55:58 +0530

    New name

---
+++ Rough/index.js
@@ -0,0 +1,72 @@
+// // let age =14;
+// // let aadharcardtrue=true;
+// // let  voteid=false;
+// // if(age>=18 && (aadharcardtrue||voteid))
+// // {
+// //     console.log("you are eligible")
+// // }
+
+// // else
+// // {
+// // console.log("you are not eligible")
+// // }
+
+// // pre increment post increament
+// // pre decrement post decremetent
+
+// // function praveen(number1,number2){
+// //     let number = number1+number2;
+// //     return number;
+
+// // }
+// // console.log(praveen(12,13))
+
+// function check(number){
+//     let result=number%2;
+// if(result == 0)
+// {
+//    console.log("numberis even number")
+    
+// }
+// else
+// {
+// console.log("number is odd number")
+// }
+// return result;
+// }
+// output=check(7)
+// console.log(output)
+
+// arrow function
+// let isEvenOrOdd = function(number) {
+//     if (number % 2 == 0) console.log(number, 'is even!');
+        
+//     else console.log(number, 'is odd!')
+    
+// }
+
+// isEvenOrOdd(6);
+
+// let a=5;
+// console.log(a);
+// a=6;
+// // console.log(typeof a);
+
+// let a = {
+//     name: "Praveen" ,
+//     age : 20 ,
+//     language: " English" ,
+
+// }
+// let b=JSON.stringify(a)
+// console.log(b)
+// console.log("Austin")
+//  Full stack website  project 
+// e commerce website 
+// learning app 
+// food website
+let a ="string"
+let b="tamil"
+console.log(a,b)
+
+
